aab7ccc080573035b697c9c705cf341430c0f3aa
Bernard Scott <bernard.scott111@gmail.com>
2024-03-03 21:46:30 -0500

    Cleaned up code by removing spurious imports, comments, and log statements. Also changing various var variables to val.

--- app/src/main/java/com/example/lunchilicious/MainActivity.kt
+++ app/src/main/java/com/example/lunchilicious/MainActivity.kt
@@ -46,7 +46,7 @@ class Repository {
         MenuItem(5, "Side", "Gravy Fries", "Fries with gravy on top", 3.95),
         MenuItem(6, "Entree", "Raspberry Chicken", "Fried chicken topped with raspberry sauce and pineapple salsa", 10.00),
         MenuItem(7, "Entree", "Dragon & Phoenix", "Shrimp w. garlic sauce & chunk chicken fried in spicy sauce", 12.00),
-        MenuItem(8, "Burrito", "Everymeat Burrito", "Chicken, beef, pork, lobster, shrimp, fish, duck, lamb, turkey, bison, cornish game hen, goose, pheasant, quail, rabbit, squab, venison, boar, alligator, antelope, caribou, elk, ostrich, turtle, rattlesnake, and kangaroo", 49.95),
+        MenuItem(8, "Burrito", "Everymeat Burrito", "Chicken, beef, pork, lobster, shrimp, fish, duck, lamb, turkey, bison, cornish game hen, goose, pheasant, qual, rabbit, squab, venison, boar, alligator, antelope, caribou, elk, ostrich, turtle, rattlesnake, and kangaroo", 49.95),
         MenuItem(9, "Hoagie", "Beer-Battered Cod Sandwich", "Fried cod filet and tartar sauce on brioche bun", 8.95),
         MenuItem(10, "Pizza", "Margherita Pizza", "Fresh tomatoes, basil and mozzarella on neopolitan crust", 14.95)
     )
